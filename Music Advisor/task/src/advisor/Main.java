bd5fb2f46ae5b6edf3d2fb8b95ef64e90af88f89
Hubert <huberto-88@o2.pl>
2021-11-25 02:25:43 +0100

    stage 4/5

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -3,6 +3,9 @@ package advisor;
 public class Main {
 
     public static void main(String[] args) {
+        if (args.length > 1 && args[0].contains("-access")) {
+            Authorization.SERVER = args[1];
+        }
         new App().startApp();
     }
 }
